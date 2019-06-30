a5888a75525f35d508f7f836fae85f4e0701876e
Unknown <h.kazuki.lovebomb@gmail.com>
2019-06-30 20:49:20 +0900

    able to calibrate

--- mount_point/ros_arduino_converter/src/float_arr_converter.py
+++ mount_point/ros_arduino_converter/src/float_arr_converter.py
@@ -7,6 +7,11 @@ from sensor_msgs.msg import JointState
 extracted_joint_list = [True, True, False, False, True, True, False, True, True, False, False, True, True, False,
                         False, True, True, False, False, False, False, False]
 
+joint_bias = [-70, 240, 180, 180, 181, 173, 180, 180, 165, 180,
+              180, 188, 180, 180, 180, 192, 248, 180, 180, 180, 180]
+joint_coef = [290, -235, -150, -150, -160, -160, -150, -140, -140, -150,
+              -150, -160, -150, -150, -150, -170, -290, -150, -150, -150, -150, -150]
+
 
 def converter_callback(msg):
     pos = []
@@ -17,7 +22,7 @@ def converter_callback(msg):
         exit(1)
 
     send_msg = Float32MultiArray()
-    send_msg.data = [pos[i] for i in range(22) if extracted_joint_list[i]]
+    send_msg.data = [pos[i]*joint_coef[i]+joint_bias[i] for i in range(22) if extracted_joint_list[i]]
 
     pub.publish(send_msg)
 
